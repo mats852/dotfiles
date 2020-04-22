7c2ef9286291c147282f465a0e3acca4b02761c6
Mathieu St-Vincent <m@mats.dev>
2020-04-21 21:31:38 -0400

    Added i3 config for kde

---
+++ kde/env/path.sh
@@ -0,0 +1,15 @@
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/bin" ] ; then
+    PATH="$HOME/bin:$PATH"
+fi
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/.local/bin" ] ; then
+    PATH="$HOME/.local/bin:$PATH"
+fi
+
+# set PATH so it includes snap bin if it exists
+if [ -d "/snap/bin" ] ; then
+    PATH="/snap/bin:$PATH"
+fi
+
