99e7ec46e7aa1d0bd955338398191191f4d8b4aa
Mathieu St-Vincent <m@mats.dev>
2021-07-31 14:52:10 -0400

    Updated tmux conf and dynamic arguments setup script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-projects=(
-  bash
-  vim
-  nvim
-)
-
 echo "Symlinking configurations..."
 
-for project in "${projects[@]}"; do
+if [ $# -eq 0 ]; then
+    echo "Error: No apps specified, e.g tmux nvim"
+    exit 1
+fi
+
+for project in "$@"; do
+  if [ ! -d "$project" ]; then
+    printf "%12s ...... error: project \"$project\" missing\n" $project
+    continue
+  fi
 
   INSTALL="$(pwd)/$project/link.sh"
   
--- tmux/link.sh
+++ tmux/link.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)
-DEST="$HOME/.config/nvim"
-FILE="init.vim"
+DEST="$HOME"
+FILE=".tmux.conf"
 
 mkdir -p $DEST
 ln -sf "$DIR/$FILE" "$DEST/$FILE"
