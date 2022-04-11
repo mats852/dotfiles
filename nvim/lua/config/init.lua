8659d80b0dc041c5d4571d9a749654a4d0c8c2ee
Mathieu St-Vincent <m@mats.dev>
2022-04-10 23:17:41 -0400

    pesky tabs

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -1,9 +1,9 @@
 -- Shorthand to create bindings
 function CreateNoremap(type, opts)
-	return function(lhs, rhs, bufnr)
-		bufnr = bufnr or 0
-		vim.api.nvim_buf_set_keymap(bufnr, type, lhs, rhs, opts)
-	end
+  return function(lhs, rhs, bufnr)
+    bufnr = bufnr or 0
+    vim.api.nvim_buf_set_keymap(bufnr, type, lhs, rhs, opts)
+  end
 end
 
 Nnoremap = CreateNoremap("n", { noremap = true })
