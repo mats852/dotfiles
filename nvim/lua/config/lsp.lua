db59eba30a00bb872e520928b169e4741918affd
Mathieu St-Vincent <m@mats.dev>
2022-04-10 23:14:36 -0400

    removed deprecated setting

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -3,11 +3,6 @@ local nvim_lsp = require('lspconfig')
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-  -- Enable completion triggered by <c-x><c-o>
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   Nnoremap('gD', ':lua vim.lsp.buf.declaration()<CR>')
   Nnoremap('gd', ':vsplit <bar> lua vim.lsp.buf.definition()<CR>')
