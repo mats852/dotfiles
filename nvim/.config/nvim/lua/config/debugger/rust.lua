e610f9526efdffcae0dee333a10c27723329e652
Mathieu St-Vincent <mathieu.st-vincent@qohash.com>
2025-09-25 10:10:06 -0400

    added rust debugger

---
+++ nvim/.config/nvim/lua/config/debugger/rust.lua
@@ -0,0 +1,27 @@
+local dap = require('dap')
+
+dap.configurations.rust = {
+  {
+    -- ... the previous config goes here ...,
+    initCommands = function()
+      -- Find out where to look for the pretty printer Python module
+      local rustc_sysroot = vim.fn.trim(vim.fn.system('rustc --print sysroot'))
+
+      local script_import = 'command script import "' .. rustc_sysroot .. '/lib/rustlib/etc/lldb_lookup.py"'
+      local commands_file = rustc_sysroot .. '/lib/rustlib/etc/lldb_commands'
+
+      local commands = {}
+      local file = io.open(commands_file, 'r')
+      if file then
+        for line in file:lines() do
+          table.insert(commands, line)
+        end
+        file:close()
+      end
+      table.insert(commands, 1, script_import)
+
+      return commands
+    end,
+    -- ...,
+  }
+}
